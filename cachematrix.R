1d6c9f8f1920624b217f5115f9cffb65ac471916
chu1011 <chu1011@gmail.com>
2016-02-16 06:45:54 +0900

    upload

---
+++ cachematrix.R
@@ -0,0 +1,25 @@
+## Put comments here that give an overall description of what your
+## functions do
+
+## Write a short comment describing this function
+
+#test
+a <- matrix(c(1,3,4,5,6,7,8,9,10), 3, 3)
+a
+b <- solve(a)
+b
+c <- a %*% b
+round(c, digits =0)
+
+
+
+makeCacheMatrix <- function(x = matrix()) {
+
+}
+
+
+## Write a short comment describing this function
+
+cacheSolve <- function(x, ...) {
+        ## Return a matrix that is the inverse of 'x'
+}
